a6ac8c6dae562978007667583aa602f5109acd1f
AlexCLeduc <ledualex@gmail.com>
2023-07-28 15:07:03 -0400

    bump version for changelog features

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-versionator",
-    version="0.0.1b",
+    version="0.2.0b",
     author="AlexCLeduc",
     # author_email="author@example.com",
     # description="A small example package",
@@ -13,12 +13,21 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/AlexCleduc/django-versionator",
     packages=[
-        # find_packages() also includes extraneous stuff, like testing and django_sample
+        # find_packages() also includes extraneous stuff, like testing and sample_app
         package
         for package in setuptools.find_packages()
         if package.startswith("versionator")
     ],
     install_requires=[],
+    # to install these subdeps, use pip install django-versionator[changelog]
+    extras_require={
+        "changelog": [
+            "pleasant-promises>=1.1",
+            "graphene>=3.1.1, <4",
+            "graphene-django>=3, <4",
+            "graphql-core-promise>=3.2.3,<4",
+        ],
+    },
     tests_require=["django"],
     classifiers=[
         "Programming Language :: Python :: 3",
