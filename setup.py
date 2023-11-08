8a5448936ee12d6942eedd13da98e413692dcb65
AlexCLeduc <ledualex@gmail.com>
2023-11-08 13:57:12 -0500

    deploy 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-versionator",
-    version="1.0.0",
+    version="1.1.0",
     author="AlexCLeduc",
     # author_email="author@example.com",
     # description="A small example package",
